dda010c53928e1192736bac5a748d88893de753c
Serrones <fabioserrones@gmail.com>
2018-09-16 19:44:20 -0300

    Aula 03 Flask Drops

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask
+import db
+from flask import Flask, abort, url_for
 
 app =Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return 'Hello World'
+@app.route('/')
+def index():
+    html = ['<ul>'] 
+    for username, user in db.users.items():
+        html.append(
+            f"<li><a href='{url_for('user', username=username)}'>{user['name']}</a></li>"
+        )
+    html.append('</ul>')
+    return '\n'.join(html) 
 
-app.run()
+
+def profile(username):
+    user = db.users.get(username)
+
+    if user:
+        return f"""
+            <h1>{user['name']}</h1></br>
+            <h2>{user['age']}</h2></br>
+            <h3>{user['tel']}</h3></br>
+            <a href="/">Voltar</a>
+        """
+    else:
+        return abort(404, "User not found")
+# Como não utilizamos o @app.route para a view profile, utilizamos o add_url_rule
+app.add_url_rule('/user/<username>', view_func=profile, endpoint='user')
+
+app.run(use_reloader=True)
---
+++ db.py
@@ -0,0 +1,12 @@
+users = {
+    "Michael": {
+        "name": "Michael Scott",
+        "age": 20,
+        "tel": "555-12345"  
+    },
+    "Christian": {
+        "name": "Christian Smith",
+        "age": 30,
+        "tel": "555-55345"
+    }
+}
